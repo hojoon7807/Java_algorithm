cb9bfab16ca8bd840bad748ffd4205aca90a4e7b
hojoon <ujuj0202@gmail.com>
2022-08-31 14:31:35 +0900

    Beakjoon 14889 시간초과 해결

--- src/baekjoon/B14889.java
+++ src/baekjoon/B14889.java
@@ -3,13 +3,12 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class B14889 {
-
   static int[][] arr;
   static int N;
   static boolean[] isVisited;
-
   static int min = Integer.MAX_VALUE;
 
   public static void main(String[] args) throws IOException {
@@ -20,10 +19,10 @@ public class B14889 {
     isVisited = new boolean[N];
 
     for (int i = 0; i < N; i++) {
-      String[] strings = br.readLine().split(" ");
+      StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
       for (int j = 0; j < N; j++) {
-        arr[i][j] = Integer.parseInt(strings[j]);
+        arr[i][j] = Integer.parseInt(st.nextToken());
       }
     }
 
@@ -32,16 +31,16 @@ public class B14889 {
     System.out.println(min);
   }
 
-  private static void dfs(int num, int depth) {
+  static void dfs(int index, int depth) {
     if (depth == N / 2) {
       cal();
       return;
     }
 
-    for (int i = num; i < N; i++) {
+    for (int i = index; i < N; i++) {
       if (!isVisited[i]) {
         isVisited[i] = true;
-        dfs(num + 1, depth + 1);
+        dfs(i + 1, depth + 1);
         isVisited[i] = false;
       }
     }
@@ -53,16 +52,19 @@ public class B14889 {
 
     for (int i = 0; i < N - 1; i++) {
       for (int j = i + 1; j < N; j++) {
-        if (isVisited[i] == true && isVisited[j] == true) {
-          start += arr[i][j];
-          start += arr[j][i];
-        } else if (isVisited[i] == false && isVisited[j] == false) {
-          link += arr[i][j];
-          link += arr[j][i];
+        if (isVisited[i] && isVisited[j]) {
+          start += (arr[i][j] + arr[j][i]);
+        } else if (!isVisited[i] && !isVisited[j]) {
+          link += (arr[i][j] + arr[j][i]);
         }
       }
     }
     int val = Math.abs(start - link);
+
+    if (val == 0) {
+      System.out.println(val);
+      System.exit(0);
+    }
 
     min = Math.min(val, min);
   }
