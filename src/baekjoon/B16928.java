9895e3ddbe51799eec0becfa5cf2c3d1de802696
hojoon <ujuj0202@gmail.com>
2022-09-06 03:54:36 +0900

    Beakjoon 16928 풀이 실패

---
+++ src/baekjoon/B16928.java
@@ -0,0 +1,87 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
+public class B16928 {
+
+  static int[] map = new int[101];
+  static boolean[] isVisited = new boolean[101];
+  static int[] count = new int[101];
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String[] strings = br.readLine().split(" ");
+
+    int N = Integer.parseInt(strings[0]);
+    int M = Integer.parseInt(strings[1]);
+
+    for (int i = 0; i < N; i++) {
+      String[] xy = br.readLine().split(" ");
+
+      map[Integer.parseInt(xy[0])] = Integer.parseInt(xy[1]);
+    }
+
+    for (int i = 0; i < M; i++) {
+      String[] uv = br.readLine().split(" ");
+
+      map[Integer.parseInt(uv[0])] = Integer.parseInt(uv[1]);
+    }
+
+    bfs();
+  }
+
+  static void bfs() {
+    LinkedList<Integer> queue = new LinkedList<>();
+
+    queue.add(1);
+    isVisited[1] = true;
+
+    while (!queue.isEmpty()) {
+      int recent = queue.remove();
+
+      if (recent == 100) {
+        System.out.println(count[recent]);
+        return;
+      }
+
+      for (int j = 1; j < 7; j++) {
+        int next = recent + j;
+        if (next > 100) {
+          continue;
+        }
+
+        if (isVisited[next]) {
+          continue;
+        }
+        isVisited[next] = true;
+
+
+        if (map[next] != 0) {
+          if (!isVisited[map[next]]) {
+            isVisited[map[next]] = true;
+            queue.add(map[next]);
+            count[map[next]] = count[recent] + 1;
+            continue;
+          }
+        }
+
+
+        count[next] = count[recent] + 1;
+        queue.add(next);
+      }
+    }
+  }
+
+  static class Node{
+    int x;
+    int count;
+
+    public Node(int x, int count) {
+      this.x = x;
+      this.count = count;
+    }
+  }
+}
