7f9e65d67640350aab26651077a3ef352a618b2c
hojoon <ujuj0202@gmail.com>
2022-09-19 21:51:51 +0900

    Beakjoon 11729 풀이

---
+++ src/baekjoon/B11729.java
@@ -0,0 +1,32 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B11729 {
+  static int K;
+  static StringBuilder sb = new StringBuilder("");
+  public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    hanoi(N,1,3,2);
+    System.out.println(K);
+    System.out.println(sb.toString());
+  }
+
+  static void hanoi(int n,int start, int end, int axis) {
+    K ++;
+    if (n == 1) {
+      sb.append(start + " " + end + "\n");
+      //System.out.println(start + " " + end);
+      return;
+    }
+
+    hanoi(n - 1, start, axis, end);
+    sb.append(start + " " + end + "\n");
+//    System.out.println(start + " " + end);
+    hanoi(n - 1, axis, end, start);
+  }
+
+}
