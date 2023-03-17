30d0f89ae43b6bb651c0a75900d373e82a204e7b
hojoon <ujuj0202@gmail.com>
2023-03-17 17:07:21 +0900

    백준 12886

--- src/baekjoon/B12886.java
+++ src/baekjoon/B12886.java
@@ -3,7 +3,7 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
+import java.util.Arrays;
 import java.util.LinkedList;
 
 public class B12886 {
@@ -13,13 +13,12 @@ public class B12886 {
     String[] input = br.readLine().split(" ");
     int[] abc = new int[3];
 
-    HashSet<int[]> set = new HashSet<>();
-
     boolean[][] isVisited = new boolean[1501][1501];
 
     for (int i = 0; i < 3; i++) {
       abc[i] = Integer.parseInt(input[i]);
     }
+    Arrays.sort(abc);
 
     if ((abc[0] + abc[1] + abc[2]) % 3 != 0) {
       System.out.println(0);
@@ -27,8 +26,46 @@ public class B12886 {
     }
 
     LinkedList<int[]> q = new LinkedList<>();
-    q.add(new int[]{abc[0], abc[1], abc[2]});
+
+    q.add(abc);
     isVisited[abc[0]][abc[1]] = true;
+
+    while (!q.isEmpty()) {
+      int[] recent = q.poll();
+      int a = recent[0];
+      int b = recent[1];
+      int c = recent[2];
+
+      if (a == b && b == c) {
+        System.out.println(1);
+        return;
+      }
+
+      if (a != b) {
+        int na = a * 2;
+        int nb = b - a;
+        int[] nArr = {na, nb, c};
+        Arrays.sort(nArr);
+        if (!isVisited[nArr[0]][nArr[1]]) {
+          isVisited[nArr[0]][nArr[1]] = true;
+          q.add(nArr);
+        }
+      }
+
+      if (a != c) {
+        int na = a * 2;
+        int nc = c - a;
+        int[] nArr = {na, b, nc};
+        Arrays.sort(nArr);
+        if (!isVisited[nArr[0]][nArr[1]]) {
+          isVisited[nArr[0]][nArr[1]] = true;
+          q.add(nArr);
+        }
+      }
+    }
+    System.out.println(0);
+
   }
+
 }
 
