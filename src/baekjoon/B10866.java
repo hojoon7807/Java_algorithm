dba4db58f437dcca94dda10e0b9fecc418425916
hojoon <ujuj0202@gmail.com>
2023-05-08 23:24:55 +0900

    백준 10866

---
+++ src/baekjoon/B10866.java
@@ -0,0 +1,72 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
+public class B10866 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(br.readLine());
+    LinkedList<Integer> deque = new LinkedList<>();
+
+    String[] input;
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < N; i++) {
+      input = br.readLine().split(" ");
+      String command = input[0];
+
+      switch (command) {
+        case "push_back":
+          deque.add(Integer.parseInt(input[1]));
+          break;
+        case "push_front":
+          deque.addFirst(Integer.parseInt(input[1]));
+          break;
+        case "pop_front":
+          if (deque.isEmpty()) {
+            sb.append(-1 + "\n");
+          } else {
+            sb.append(deque.pollFirst() + "\n");
+          }
+          break;
+        case "pop_back":
+          if (deque.isEmpty()) {
+            sb.append(-1 + "\n");
+          } else {
+            sb.append(deque.pollLast() + "\n");
+          }
+          break;
+        case "size":
+          sb.append(deque.size() + "\n");
+          break;
+        case "empty":
+          if (deque.isEmpty()) {
+            sb.append(1 + "\n");
+          } else {
+            sb.append(0 + "\n");
+          }
+          break;
+        case "front":
+          if (deque.isEmpty()) {
+            sb.append(-1 + "\n");
+          } else {
+            sb.append(deque.peekFirst() + "\n");
+          }
+          break;
+        case "back":
+          if (deque.isEmpty()) {
+            sb.append(-1 + "\n");
+          } else {
+            sb.append(deque.peekLast() + "\n");
+          }
+          break;
+      }
+    }
+    System.out.println(sb);
+  }
+
+}
