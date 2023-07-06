24842f6b0c01e6add66c46f0b195e24dfb59f161
hojoon <ujuj0202@gmail.com>
2023-07-07 00:01:37 +0900

    백준 14502

--- src/baekjoon/B14502.java
+++ src/baekjoon/B14502.java
@@ -10,8 +10,8 @@ public class B14502 {
   static int n, m;
   static int[][] map;
   static int max = Integer.MIN_VALUE;
-  static int[] dx = {1, -1, 0, 0};
-  static int[] dy = {0, 0, 1, -1};
+  static int[] dr = {1, -1, 0, 0};
+  static int[] dc = {0, 0, 1, -1};
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,85 +23,85 @@ public class B14502 {
     for (int i = 0; i < n; i++) {
       String[] shape = br.readLine().split(" ");
       for (int j = 0; j < m; j++) {
-        map[i][j] = Integer.parseInt(shape[j]);
+        int status = Integer.parseInt(shape[j]);
+        map[i][j] = status;
       }
     }
 
-    dfs(0);
+    dfs(0,0);
     System.out.println(max);
   }
 
-  static void dfs(int depth) {
-    if (depth == 3) {
+  private static void dfs(int count, int start) {
+    if (count == 3) {
       bfs();
       return;
     }
+    for (int i = start; i < n * m; i++) {
+      int r = i / m;
+      int c = i % m;
 
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
-        if (map[i][j] == 0) {
-          map[i][j] = 1;
-          dfs(depth + 1);
-          map[i][j] = 0;
-        }
+      if (map[r][c] == 0) {
+        map[r][c] = 1;
+        dfs(count + 1, i + 1);
+        map[r][c] = 0;
       }
     }
+
   }
 
-  static void bfs() {
+  private static void bfs() {
     LinkedList<Node> q = new LinkedList<>();
-
+    boolean[][] isVisited = new boolean[n][m];
 
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < m; j++) {
         if (map[i][j] == 2) {
           q.add(new Node(i, j));
+          isVisited[i][j] = true;
         }
       }
     }
 
     while (!q.isEmpty()) {
-      Node now = q.remove();
-      for (int i = 0; i < 4; i++) {
-        int nx = now.x + dx[i];
-        int ny = now.y + dy[i];
+      Node poll = q.poll();
 
-        if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
-          if (map[nx][ny] == 0) {
-            map[nx][ny] = 3;
-            q.add(new Node(nx, ny));
-          }
+      for (int i = 0; i < 4; i++) {
+        int nr = poll.r + dr[i];
+        int nc = poll.c + dc[i];
+
+        if (nr >= 0 && nr < n && nc >= 0 && nc < m && !isVisited[nr][nc]
+            && map[nr][nc] == 0) {
+          isVisited[nr][nc] = true;
+          map[nr][nc] = 3;
+          q.add(new Node(nr, nc));
         }
       }
     }
-    cal();
-  }
 
-  static void cal() {
-    int count = 0;
-
+    int safetyZone = 0;
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < m; j++) {
         if (map[i][j] == 0) {
-          count++;
-        }
-
-        else if (map[i][j] == 3) {
+          safetyZone++;
+        } else if (map[i][j] == 3) {
           map[i][j] = 0;
         }
       }
     }
-    max = Math.max(count, max);
+
+    max = Math.max(max, safetyZone);
+
   }
 
   static class Node {
 
-    int x;
-    int y;
+    int r;
+    int c;
 
-    public Node(int x, int y) {
-      this.x = x;
-      this.y = y;
+    public Node(int r, int c) {
+      this.r = r;
+      this.c = c;
     }
   }
 }
