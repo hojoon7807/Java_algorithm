b7abb0f813577f510aad87d5bb4e9fb7dcb41f8d
hojoon <ujuj0202@gmail.com>
2024-01-15 00:42:42 +0900

    백준 3584

--- src/baekjoon/B3584.java
+++ src/baekjoon/B3584.java
@@ -3,14 +3,13 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class B3584 {
 
   static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
   static int t, n;
-  static ArrayList<ArrayList<Integer>> tree = new ArrayList<>();
+  static int[] parents;
   static boolean[] isVisited;
   static int answer = 0;
 
@@ -25,51 +24,32 @@ public class B3584 {
     while (t-- > 0) {
       n = Integer.parseInt(br.readLine());
       isVisited = new boolean[n + 1];
-
-      for (int i = 0; i <= n; i++) {
-        tree.add(new ArrayList<>());
-      }
+      parents = new int[n + 1];
 
       for (int i = 0; i < n - 1; i++) {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
 
-        tree.get(b).add(a);
+        parents[b] = a;
       }
 
       StringTokenizer st = new StringTokenizer(br.readLine());
       int node = Integer.parseInt(st.nextToken());
       int node2 = Integer.parseInt(st.nextToken());
 
-      isVisited[node] = true;
-      dfs(node);
-      isVisited[node2] = true;
-      dfs(node2);
+      while (node > 0) {
+        isVisited[node] = true;
+        node = parents[node];
+      }
 
-      sb.append(answer).append("\n");
+      while (node2 > 0 && !isVisited[node2]) {
+        node2 = parents[node2];
+      }
 
-      answer = 0;
-      tree.clear();
+      sb.append(node2).append("\n");
     }
 
     System.out.println(sb);
   }
-
-  private static void dfs(int v){
-    if (tree.get(v).size() == 0) {
-      return;
-    }
-
-    for (Integer next : tree.get(v)) {
-      if (!isVisited[next]) {
-        isVisited[next] = true;
-        dfs(next);
-      } else {
-        answer = next;
-        return;
-      }
-    }
-  }
-
 }
