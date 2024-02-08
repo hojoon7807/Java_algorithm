df4f9af0b508d7ad4ed5478850ebc70d6ea93ca6
hojoon <ujuj0202@gmail.com>
2024-02-08 19:38:08 +0900

    프로그래머스 [후보키](https://school.programmers.co.kr/learn/courses/30/lessons/42890)

--- src/programmers/CandidateKey.java
+++ src/programmers/CandidateKey.java
@@ -1,74 +1,87 @@
 package programmers;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class CandidateKey {
 
   public static void main(String[] args) {
     System.out.println(
-        solution(new String[][]{{"a", "1", "aaa", "c", "ng"},
-            {"a", "1", "bbb", "e", "g"},
-            {"c", "1", "aaa", "d", "ng"},
-            {"d", "2", "bbb", "d", "ng"}}));
+        solution(new String[][]{
+            {"100", "ryan", "music", "2"},
+            {"200", "apeach", "math", "2"},
+            {"300", "tube", "computer", "3"},
+            {"400", "con", "computer", "4"},
+            {"500", "muzi", "music", "3"},
+            {"600", "apeach", "music", "2"}}));
   }
 
-  static int[] isVisited;
-  static int colLen, rowLen;
-  static Set<Set<Integer>> answerSet = new HashSet<>();
+  static int rowLen;
+  static int colLen;
+  static HashSet<String> combineSet = new HashSet();
+  static List<Set<Integer>> keySet = new ArrayList();
+  static int answer = 0;
 
   public static int solution(String[][] relation) {
-    colLen = relation[0].length;
+    // 컬럼의 최대 조합 개수 2^8 - 1
     rowLen = relation.length;
-    isVisited = new int[colLen];
+    colLen = relation[0].length;
 
-    for (int i = 0; i < colLen; i++) {
-      combine(0, i + 1, relation, new HashSet<>(), 0);
+    for (int i = 1; i <= colLen; i++) {
+      combine(0, i, 0, new HashSet<Integer>(), relation);
     }
-    return answerSet.size();
+
+    System.out.println(answer);
+
+    return answer;
   }
 
-  static void combine(int depth, int len, String[][] relation, Set<Integer> key, int start) {
-    if (depth == len) {
-      if (isCandidate(relation)) {
-        for (Set<Integer> savedKey : answerSet) {
-          if (key.containsAll(savedKey)) {
-            return;
-          }
-        }
-        answerSet.add(key);
+  static void combine(int depth, int select, int index, HashSet<Integer> key, String[][] relation) {
+    if (depth == select) {
+      if (checkKey(key, relation)) {
+        answer++;
+        keySet.add(key);
       }
+
+      combineSet.clear();
       return;
     }
 
-    for (int i = start; i < colLen; i++) {
-      isVisited[i] = 1;
-      key.add(i);
-      combine(depth + 1, len, relation, key, i + 1);
-      isVisited[i] = 0;
+    for (int i = index; i < colLen; i++) {
+      HashSet<Integer> newKey = new HashSet<>(key);
+      newKey.add(i);
+      combine(depth + 1, select, i + 1, newKey, relation);
     }
   }
 
-
-  static boolean isCandidate(String[][] relation) {
-    Set<String> set = new HashSet();
-
+  static boolean checkKey(HashSet<Integer> key, String[][] relation) {
+    if (!isMinimal(key)) {
+      return false;
+    }
     for (int i = 0; i < rowLen; i++) {
-      StringBuilder sb = new StringBuilder();
-      for (int j = 0; j < colLen; j++) {
-        if (isVisited[j] == 1) {
-          sb.append(relation[i][j]);
-        }
+      String combine = "";
+      for (Integer k : key) {
+        combine += relation[i][k];
       }
-
-      if (set.contains(sb.toString())) {
-        return false;
+      if (!combineSet.contains(combine)) {
+        combineSet.add(combine);
       } else {
-        set.add(sb.toString());
+        return false;
       }
     }
-
     return true;
   }
+
+  static boolean isMinimal(HashSet<Integer> key) {
+    for (Set<Integer> minimal : keySet) {
+      if (key.containsAll(minimal)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
 
 }
