77a1d20715690316b9b0d0afd8dedb1b80bc3eea
HoJoon Lim <ujuj0202@gmail.com>
2024-03-30 10:05:55 +0900

    [Silver IV] Title: 듣보잡, Time: 308 ms, Memory: 24112 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,57 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    String[] nm = br.readLine().split(" ");
+
+    int n = Integer.parseInt(nm[0]);
+    int m = Integer.parseInt(nm[1]);
+
+    String[] arrN = new String[n];
+    String[] arrM = new String[m];
+
+    for (int i = 0; i < n; i++) {
+      arrN[i] = br.readLine();
+    }
+
+    for (int i = 0; i < m; i++) {
+      arrM[i] = br.readLine();
+    }
+
+    Arrays.sort(arrN);
+    Arrays.sort(arrM);
+
+    int count = 0;
+    StringBuilder sb = new StringBuilder();
+    for (String person : arrN) {
+      int start = 0;
+      int end = m-1;
+      while (start <= end) {
+        int mid = (start+end) >> 1;
+
+        String target = arrM[mid];
+        if(target.equals(person)){
+          count ++;
+          sb.append(target).append("\n");
+          break;
+        }
+
+        if (person.compareTo(target) > 0) {
+          start = mid + 1;
+        } else {
+          end = mid -1;
+        }
+      }
+    }
+
+    System.out.println(count);
+    System.out.println(sb);
+  }
+}
