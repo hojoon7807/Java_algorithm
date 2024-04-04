dad3b137e0a613dbcb5ac3549d04fbaaf4199bea
HoJoon Lim <ujuj0202@gmail.com>
2024-04-04 12:01:22 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 3108 ms, Memory: 429520 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -7,7 +7,7 @@ public class Main {
 
   static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
   static int t, k;
-  static TreeMap<Long, Integer> map = new TreeMap<>();
+  static TreeMap<Integer, Integer> map = new TreeMap<>();
   static StringBuilder sb = new StringBuilder();
 
   /*
@@ -16,7 +16,6 @@ public class Main {
   최소힙, 최대 힙 사용하게 되면 각 위치를 찾아서 삭제하는 경우 때문에 시간 초과 예상
   deque 정렬 작업 추가 될 경우 nlogn 추가로 시간초과
   TreeMap 사용하면 레드블랙트리로 first last 받을 수 있음
-  2^31 은 Integer 범위 초과
    */
   public static void main(String[] args) throws IOException {
     t = Integer.parseInt(br.readLine());
@@ -36,7 +35,7 @@ public class Main {
     for (int i = 0; i < k; i++) {
       String[] input = br.readLine().split(" ");
       String command = input[0];
-      long value = Long.parseLong(input[1]);
+      int value = Integer.parseInt(input[1]);
 
       switch (command) {
         case "I":
@@ -44,7 +43,7 @@ public class Main {
           break;
         case "D":
           // 최댓값 제거
-          if (value == 1L) {
+          if (value == 1) {
             deleteMax();
           } else {
             deleteMin();
@@ -54,7 +53,7 @@ public class Main {
     print();
   }
 
-  static void print(){
+  static void print() {
     if (map.isEmpty()) {
       sb.append("EMPTY");
     } else {
@@ -66,10 +65,12 @@ public class Main {
     if (map.isEmpty()) {
       return;
     }
-    Long maxKey = map.lastKey();
-    map.put(maxKey, map.get(maxKey) - 1);
-    if (map.get(maxKey) == 0) {
+    Integer maxKey = map.lastKey();
+
+    if (map.get(maxKey) == 1) {
       map.remove(maxKey);
+    } else {
+      map.replace(maxKey, map.get(maxKey) - 1);
     }
   }
 
@@ -77,10 +78,12 @@ public class Main {
     if (map.isEmpty()) {
       return;
     }
-    Long minKey = map.firstKey();
-    map.put(minKey, map.get(minKey) - 1);
-    if (map.get(minKey) == 0) {
+    Integer minKey = map.firstKey();
+
+    if (map.get(minKey) == 1) {
       map.remove(minKey);
+    } else {
+      map.replace(minKey, map.get(minKey) - 1);
     }
   }
 
