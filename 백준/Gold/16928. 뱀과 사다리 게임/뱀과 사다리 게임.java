7a794f0401193079a867c2e962d52c7615766c8f
HoJoon Lim <ujuj0202@gmail.com>
2024-04-08 12:31:46 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 128 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
+public class Main {
+
+  static int[] map = new int[101];
+  static boolean[] isVisited = new boolean[101];
+  static int[] count = new int[101];
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String[] strings = br.readLine().split(" ");
+
+    int N = Integer.parseInt(strings[0]);
+    int M = Integer.parseInt(strings[1]);
+
+    for (int i = 0; i < N+M; i++) {
+      String[] xy = br.readLine().split(" ");
+
+      map[Integer.parseInt(xy[0])] = Integer.parseInt(xy[1]);
+    }
+
+    bfs();
+  }
+
+  static void bfs() {
+    LinkedList<Integer> queue = new LinkedList<>();
+
+    queue.add(1);
+    isVisited[1] = true;
+
+    while (!queue.isEmpty()) {
+      int recent = queue.remove();
+
+      for (int j = 1; j < 7; j++) {
+        int next = recent + j;
+
+        if (next == 100) {
+          System.out.println(count[recent] + 1);
+          return;
+        }
+
+        if (next < 101 && count[next] == 0) {
+          if (map[next] != 0 && count[map[next]] == 0) {
+            queue.add(map[next]);
+            count[map[next]] = count[recent] + 1;
+          } else if(map[next]==0){
+            count[next] = count[recent] + 1;
+            queue.add(next);
+          }
+        }
+      }
+    }
+  }
+
+}
