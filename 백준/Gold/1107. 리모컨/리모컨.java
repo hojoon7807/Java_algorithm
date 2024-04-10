54534c38a2779baec7f782a8b73dad8cc0441e5a
HoJoon Lim <ujuj0202@gmail.com>
2024-04-10 11:12:11 +0900

    [Gold V] Title: 리모컨, Time: 236 ms, Memory: 44336 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.java
+++ 백준/Gold/1107. 리모컨/리모컨.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,59 +6,64 @@ public class Main {
 
   static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
   static int n, m;
-  static int nLen;
-  static boolean[] remote = new boolean[10];
-  static int answer;
+  static int min = Integer.MAX_VALUE;
+  static boolean[] broken = new boolean[10];
+
+  /*
+  채널 이동 방법
+  1. 버튼을 눌러서 바로가냐
+  2. 버튼을 눌러서 임의의 채널로 이동 후 + or -
+  3. 그냥 + , -
+
+  초기 min = n - 현재 100
+  0 <= n <= 500,000 이므로 999,999 까지 탐색
+  반복문 돌면서 해당 채널로 이동할 수 있는지 체크
+  이동가능하면 min 갱신
+  이동 불가능하면 continue;
+   */
 
   public static void main(String[] args) throws IOException {
-    input();
-    solution();
-  }
-
-  static void input() throws IOException {
-    String sn = br.readLine();
-    nLen = sn.length();
-    n = Integer.parseInt(sn);
+    n = Integer.parseInt(br.readLine());
     m = Integer.parseInt(br.readLine());
-    if (m > 0) {
-      String[] split = br.readLine().split(" ");
 
-      for (int i = 0; i < m; i++) {
-        remote[Integer.parseInt(split[i])] = true;
+    String[] input = null;
+    
+    if (m != 0) {
+      input = br.readLine().split(" ");
+    }
+
+    for (int i = 0; i < m; i++) {
+      broken[Integer.parseInt(input[i])] = true;
+    }
+
+    min = Math.abs(n - 100);
+
+    for (int i = 0; i <= 999999; i++) {
+      if (isBroken(i)) {
+        continue;
+      }
+
+      int len = Integer.toString(i).length();
+      min = Math.min(min, len + Math.abs(n - i));
+    }
+
+    System.out.println(min);
+  }
+
+  static boolean isBroken(int channel) {
+    while (true) {
+      if (broken[channel % 10]) {
+        return true;
+      }
+
+      channel /= 10;
+
+      if (channel == 0) {
+        break;
       }
     }
+
+    return false;
   }
 
-  static void solution() {
-    // 채널을 입력해서 변경하려면 적어도 n의 길이만큼 눌러야됌
-    // init = 100  103  100자리까지 3번에 가능, 98 2자리 1자
-    // + - 를 이용해 옮기는 경우
-    answer = Math.abs(n - 100);
-
-    search(0, "");
-
-    System.out.println(answer);
-
-    // 9999
-
-    // 2 3 4 5 6 7 8 9
-  }
-
-  static void search(int depth, String s) {
-    if (depth > 0) {
-      int curChannel = Integer.parseInt(s);
-      int count = Math.abs(curChannel - n) + Integer.toString(curChannel).length();
-      answer = Math.min(answer, count);
-    }
-
-    if (depth == nLen + 1) {
-      return;
-    }
-
-    for (int i = 0; i <= 9; i++) {
-      if (!remote[i]) {
-        search(depth + 1, s + i);
-      }
-    }
-  }
 }
