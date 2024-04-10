78dc3b7fb0d9e7b20066956fcf09d3913120cdb3
HoJoon Lim <ujuj0202@gmail.com>
2024-04-10 16:28:23 +0900

    [Gold IV] Title: 빙산, Time: 492 ms, Memory: 137340 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -1,101 +1,109 @@
-import java.util.*;
-import java.io.*;
- 
-// https://www.acmicpc.net/problem/2573
- 
-class Main {
-    static int r;
-    static int c;
-    static int[][] map;
-    static int[][] visited;
-    static int[][] melt;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
- 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        r = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
-        map = new int[r][c];
-        visited = new int[r][c];
-        melt = new int[r][c];
- 
-        for(int i=0; i<r; i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j=0; j<c; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
- 
-        solution();
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+  static int n, m;
+  static int[][] map;
+  static boolean[][] isVisited;
+  static int[][] melt;
+  static int[] dr = {1, -1, 0, 0};
+  static int[] dc = {0, 0, 1, -1};
+
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+  public static void main(String[] args) throws Exception {
+    String[] nm = br.readLine().split(" ");
+    n = Integer.parseInt(nm[0]);
+    m = Integer.parseInt(nm[1]);
+
+    map = new int[n][m];
+    melt = new int[n][m];
+
+    for (int i = 0; i < n; i++) {
+      map[i] = Arrays.stream(br.readLine().split(" "))
+                        .mapToInt(Integer::parseInt)
+                        .toArray();
     }
- 
-    static void solution() {
-        int year = 0;
- 
-        while(true) {
-            // dfs 로 빙산 덩어리 세기
-            int count = 0;
-            for(int i=0; i<r; i++) {
-                for(int j=0; j<c; j++) {
-                    if(visited[i][j] == 0 && map[i][j] != 0) {
-                        dfs(i, j);
-                        count++;
-                    }
-                }
-            }
- 
-            if(count == 0) {
-                System.out.println(0);
-                break;
-            } else if(count >= 2) {
-                System.out.println(year);
-                break;
-            }
- 
-            melting();
-            year++;
-        }
+
+    int year = 0;
+
+    while (true) {
+      int iceCount = getIceCount();
+
+      if (iceCount == 0) {
+        System.out.println(0);
+        return;
+      } else if (iceCount >= 2) {
+        System.out.println(year);
+        return;
+      }
+
+      melt();
+      year++;
     }
-    
-    static void dfs(int x, int y) {
-        visited[x][y] = 1;
- 
-        for(int i=0; i<4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
- 
-            if(0 <= nx && nx < r && 0 <= ny && ny < c) {
-                // 1년 후에 녹는 빙산의 양 구하기
-                if(map[nx][ny] == 0)
-                    melt[x][y]++;
- 
-                // dfs 재귀
-                if(visited[nx][ny] == 0 && map[nx][ny] != 0)
-                    dfs(nx, ny);                    
-            }
+  }
+
+  static int getIceCount() {
+    isVisited = new boolean[n][m];
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        if (map[i][j] != 0 && !isVisited[i][j]) {
+          dfs(i, j);
+          count++;
         }
+      }
     }
- 
-    static void melting() {    
-        /**
-        * 1. 빙산 녹이기
-        * 2. 만약 녹인 높이가 음수가 되면 0으로 바꿔주기
-        * 3. visited 초기화
-        * 4. melt 초기화
-        */               
-        for(int i=0; i<r; i++) {
-            for(int j=0; j<c; j++) {
-                map[i][j] -= melt[i][j];
- 
-                if(map[i][j] < 0)
-                    map[i][j] = 0;
-                                    
-                visited[i][j] = 0;
-                melt[i][j] = 0;
-            }
+
+    return count;
+  }
+
+  static void dfs(int r, int c) {
+    isVisited[r][c] = true;
+
+    for (int i = 0; i < 4; i++) {
+      int nr = r + dr[i];
+      int nc = c + dc[i];
+
+      if (isRange(nr, nc)) {
+        // 이동 위치가 바다면 현재 위치의 빙산의 녹는 횟수 증가
+        if (map[nr][nc] == 0) {
+          melt[r][c]++;
         }
+
+        if (!isVisited[nr][nc] && map[nr][nc] != 0) {
+          dfs(nr, nc);
+        }
+      }
     }
+  }
+
+  static void melt() {
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        if (melt[i][j] != 0) {
+          int meltCount = melt[i][j];
+
+          if (map[i][j] < meltCount) {
+            map[i][j] = 0;
+          } else {
+            map[i][j] -= meltCount;
+          }
+        }
+      }
+    }
+
+    melt = new int[n][m];
+  }
+
+
+  static boolean isRange(int r, int c) {
+    if (r >= 0 && r < n && c >= 0 && c < m) {
+      return true;
+    }
+    return false;
+  }
 }
