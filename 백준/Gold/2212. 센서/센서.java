c22582e126d93d80f6d926fd07f192623c8cb17a
HoJoon Lim <ujuj0202@gmail.com>
2024-04-16 09:52:03 +0900

    [Gold V] Title: 센서, Time: 208 ms, Memory: 17296 KB -BaekjoonHub

--- 백준/Gold/2212. 센서/센서.java
+++ 백준/Gold/2212. 센서/센서.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,26 +7,50 @@ import java.util.PriorityQueue;
 
 public class Main {
 
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static int n, k;
+  static int[] arr;
+
+  /*
+  2
+  1 3 6 6 7 9
+
+  2 3 0 1 2
+
+  총 거리에서 차이를 빼자
+
+  5
+  3 6 7 8 10 12 14 15 18 20
+
+  3 1 1 2 2 2 1 3 2
+   */
   public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int n = Integer.parseInt(br.readLine());
-    int k = Integer.parseInt(br.readLine());
+    n = Integer.parseInt(br.readLine());
+    k = Integer.parseInt(br.readLine());
 
-    int[] sensors = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+    arr = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
 
-    Arrays.sort(sensors);
-
-    if (k >= n) {
+    if (n <= k) {
       System.out.println(0);
       return;
     }
+    
+    Arrays.sort(arr);
 
-    int answer = sensors[n-1] - sensors[0];
+    int[] diff = new int[n - 1];
+
+    for (int i = 0; i < n - 1; i++) {
+      diff[i] = arr[i + 1] - arr[i];
+    }
+
+    int answer = arr[n - 1] - arr[0];
 
     PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
 
-    for (int i = 0; i < n - 1; i++) {
-      pq.add(sensors[i + 1] - sensors[i]);
+    for (int i : diff) {
+      pq.add(i);
     }
 
     for (int i = 0; i < k - 1; i++) {
