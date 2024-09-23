c2c275444b4a80ab4661a7ce5dfb120bd89b0d63
HoJoon Lim <ujuj0202@gmail.com>
2024-09-23 13:44:16 +0900

    [Gold III] Title: K진 트리, Time: 512 ms, Memory: 54224 KB -BaekjoonHub

--- 백준/Gold/11812. K진 트리/K진 트리.java
+++ 백준/Gold/11812. K진 트리/K진 트리.java
@@ -1,45 +1,49 @@
-import java.io.*;
-import java.util.*;
-
-class Solution{
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    StringTokenizer st;
-
-    long N;
-    int K, Q;
-
-    public Solution() throws IOException {
-        st = new StringTokenizer(br.readLine());
-        N = Long.parseLong(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        Q = Integer.parseInt(st.nextToken());
-
-        for(int i = 0; i < Q; i++){
-            st = new StringTokenizer(br.readLine());
-            long x = Long.parseLong(st.nextToken());
-            long y = Long.parseLong(st.nextToken());
-
-            if(K == 1){
-                bw.write(Math.abs(x-y)+"\n");
-                continue;
-            }
-            long cnt = 0;
-
-            while(x != y){
-                long max = Math.max(x, y);
-                y = Math.min(x, y);
-                x = (max-2)/K + 1;
-                cnt++;
-            }
-            bw.write(cnt+"\n");
-        }
-        bw.close();
-    }
-}
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        new Solution();
+
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static long n, k, q;
+
+  public static void main(String[] args) throws Exception {
+    String[] input = br.readLine().split(" ");
+    n = Long.parseLong(input[0]);
+    k = Long.parseLong(input[1]);
+    q = Long.parseLong(input[2]);
+
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 0; i < q; i++) {
+      input = br.readLine().split(" ");
+
+      long x = Long.parseLong(input[0]);
+      long y = Long.parseLong(input[1]);
+
+      // k == 1 ->
+
+      if (k == 1) {
+        sb.append(Math.abs(x - y)).append("\n");
+        continue;
+      }
+
+      // 범위 = (parent * k) - (k-2) 부모 노드 * k + 1
+      // 부모가 같을 때 까지 찾고 거리 리턴
+
+      int count = 0;
+
+      while (x != y) {
+        long max = Math.max(x, y);
+        y = Math.min(x, y);
+        count ++;
+
+        x = (max - 2) / k + 1;;
+      }
+
+      sb.append(count).append("\n");
     }
+
+    System.out.println(sb);
+  }
+
 }
